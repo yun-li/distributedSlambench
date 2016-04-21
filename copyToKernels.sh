8739e0361d86d3a4a01014b3f2bccd326f9f1fe0
yun <ilyliberty@gmail.com>
2016-04-21 01:12:44 -0500

    added copyToKernels.sh

---
+++ copyToKernels.sh
@@ -0,0 +1,11 @@
+cp kernels.cpp preprocessing/kfusion/src/cpp/kernels.cpp
+cp kernels.cpp tracking/kfusion/src/cpp/kernels.cpp
+cp kernels.cpp integration/kfusion/src/cpp/kernels.cpp
+cp kernels.cpp raycasting/kfusion/src/cpp/kernels.cpp
+cp kernels.cpp SLAMBench_Single/kfusion/src/cpp/kernels.cpp
+
+cp kernels.h preprocessing/kfusion/include/kernels.h
+cp kernels.h tracking/kfusion/include/kernels.h
+cp kernels.h integration/kfusion/include/kernels.h
+cp kernels.h raycasting/kfusion/include/kernels.h
+cp kernels.h SLAMBench_Single/kfusion/include/kernels.h
